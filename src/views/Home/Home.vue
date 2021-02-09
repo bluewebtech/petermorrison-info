a5891690b57642f8e7e7a5b0fe437ce5f38f7d59
Peter Morrison <bwt.bluewebtech@gmail.com>
2021-02-09 18:48:37 -0500

    Started working on swaying fiber animationl

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -18,6 +18,15 @@ export default class Home extends Vue {
 <template>
   <BaseLayout>
     <template v-slot:content>
+      <div class="fibers">
+        <div class="polygon_5"/>
+        <div class="polygon_4"/>
+        <div class="polygon_3"/>
+        <div class="polygon_2"/>
+        <div class="polygon_7"/>
+        <div class="polygon_6"/>
+      </div>
+
       <div class="columns">
         <div class="column">
           <h1>Hello,</h1>
@@ -54,6 +63,120 @@ export default class Home extends Vue {
       font-size: 24px;
       font-weight: 600;
     }
+  }
+}
+
+.polygon_2 {
+  animation: wind 5.5s infinite;
+}
+
+.polygon_3 {
+  animation: wind 6s infinite;
+}
+
+.polygon_4 {
+  animation: wind 5s infinite;
+}
+
+.polygon_5 {
+  animation: wind 6.5s infinite;
+}
+
+.polygon_6 {
+  animation: wind 8s infinite;
+}
+
+.polygon_7 {
+  animation: wind 9s infinite;
+}
+
+@keyframes wind {
+  30% {
+    transform: rotate(5deg);
+    transform-origin: bottom;
+  }
+  30% {
+    transform: rotate(10deg);
+    transform-origin: bottom;
+  }
+  50% {
+    transform: rotate(-15deg);
+    transform-origin: bottom;
+  }
+}
+
+.fibers {
+  position: fixed;
+  bottom: -50px;
+  right: 200px;
+  width: 600px;
+  height: 90%;
+  z-index: -1;
+
+  .polygon_2 {
+    position: absolute;
+    bottom: 0;
+    right: 100px;
+    width: 100%;
+    height: 100%;
+    background: url('/polygon_2.svg');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+  }
+
+  .polygon_3 {
+    position: absolute;
+    bottom: 0;
+    left: 60px;
+    width: 100%;
+    height: 80%;
+    background: url('/polygon_3.svg');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+  }
+
+  .polygon_4 {
+    position: absolute;
+    bottom: 0;
+    left: 20px;
+    width: 100%;
+    height: 60%;
+    background: url('/polygon_4.svg');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+  }
+
+  .polygon_5 {
+    position: absolute;
+    bottom: 0;
+    left: -170px;
+    width: 100%;
+    height: 70%;
+    background: url('/polygon_5.svg');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+  }
+
+  .polygon_6 {
+    position: absolute;
+    bottom: 0;
+    left: -35px;
+    width: 100%;
+    height: 35%;
+    background: url('/polygon_6.svg');
+    background-repeat: no-repeat;
+    background-size: 40% 100%;
+  }
+
+  .polygon_7 {
+    position: absolute;
+    bottom: -30px;
+    left: 100px;
+    width: 100%;
+    height: 60%;
+    background: url('/polygon_7.svg');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
   }
 }
 </style>
