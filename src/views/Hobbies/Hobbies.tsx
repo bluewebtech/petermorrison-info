ce61727961c5876e3827e87e42c096e9d4b30d02
Peter Morrison <bwt.bluewebtech@gmail.com>
2021-07-01 11:44:32 -0400

    Removed hobbies blurb about unfinished basement because Joe made fun of it.

--- src/views/Hobbies/Hobbies.tsx
+++ src/views/Hobbies/Hobbies.tsx
@@ -8,7 +8,7 @@ const Hobbies = () => {
           <h2><span className="has-text-black">H</span>obbies</h2>
           <p>So, when I'm not coding or thinking about tech there are many things that I enjoy doing in my free time. Everyone needs to disconnect.</p>
           <p><strong>Playing Games.</strong> Of course videos games but not always. Losing yourself in a board game with my family is by far the best of the best.</p>
-          <p><strong>House work.</strong> That's right I actually enjoy knocking out honey do lists with some home improvements. So much that I did all the designing, planning and construction of our unfinished basement.</p>
+          <p><strong>House work.</strong> That's right I actually enjoy knocking out honey do lists with some home improvements.</p>
           <p><strong>Making beer.</strong> A new hobbie that I recently started is learning how to properly make homemade beer. It's such a pretty relaxing thing to do where it has become a personal goal to open up a micro brewery.</p>
           <p><strong>Music.</strong> Whether I'm listening or playing it, music will always have that special place in my heart.</p>
           <p><strong>Shooting.</strong> Wouldn't call myself a gun nut but I certainly do enjoy heading out to a range and practicing and continuing to learn and improve upon the skill of target shooting.</p>
